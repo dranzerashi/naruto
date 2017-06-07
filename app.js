5d4c3814b95c4234ff8808fc10e716c1f7e740a0
Ashith Raghunath(ashith.raghunath) <dranzer.ashi@gmail.com>
2017-06-07 10:44:33 +0530

    Basic app with route

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
+var bodyparser = require('body-parser');
 
-var chat = require('./routes/chat');
+var chat =  require('./routes/chat');
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({'extended':'false'}));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/chat', chat);
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
